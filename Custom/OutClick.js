c1e6bf7a439fa6b09ef952792a9c593f0e12667b
Abhishek Prajapati <61529737+abhi0661236@users.noreply.github.com>
2023-02-03 15:17:21 +0530

    missing ref.

--- Custom/OutClick.js
+++ Custom/OutClick.js
@@ -15,3 +15,31 @@ if (nav) {
      }
   })
 }
+
+// If there are two different navigation component [for mobile and for largescreens]
+// above will cause a reference glitch on window resize. [on real mobile devices it will not matter as they can not resize itself.
+// though this cannot be neglected.
+// so here is the solution trycatch :)
+
+
+    React.useEffect(() => {
+        if (window) {
+            const windowClick = (e) => {
+                try {
+                    // remember we have a ref for side navbar. :)
+                    if (!nav.current.contains(e.target) && !document.getElementById("hamIcon")?.contains(e.target)) {
+                        if (nav.current.classList.contains("anim-slideleft")) {
+                            nav.current.classList.remove("anim-slideleft");
+                        }
+                    }
+                } catch (error) {
+                    console.log(error); // in case you just want to checkout the errors.
+                    return window.removeEventListener("click", windowClick); // removal of events that are no longer required is ideal :)
+                }
+            }
+
+            window.addEventListener("click", (e) => windowClick(e));
+        }
+
+
+    }, [nav]);

0729e558457c2478c131932441f43b0e45d4c35e
Abhishek Prajapati <61529737+abhi0661236@users.noreply.github.com>
2023-02-13 14:05:00 +0530

    added rootMargin

--- Custom/useIntersection.js
+++ Custom/useIntersection.js
@@ -2,13 +2,16 @@
 
 import { useState, useEffect } from 'react'
 
-const useIntersection = (ref) => {
+const useIntersection = (ref, offset) => { // ref is element ref and offset is the intersection-delay in pixels
     const [isIntersecting, setIntersecting] = useState(false);
 
 
     useEffect(() => {
         const observer = new IntersectionObserver(
-            ([entry]) => setIntersecting(entry.isIntersecting)
+            ([entry]) => setIntersecting(entry.isIntersecting),
+            {
+                rootMargin: offset || "0px"
+            }
         );
 
         if(ref?.current){

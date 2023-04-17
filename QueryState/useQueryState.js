ab590c73bfd3ecb8aa895177af425efec673bea2
Abhishek Prajapati <61529737+abhi0661236@users.noreply.github.com>
2023-04-17 06:29:13 +0000

    added QueryState

---
+++ QueryState/useQueryState.js
@@ -0,0 +1,58 @@
+import qs from "qs"
+import { useCallback, useState, useEffect } from "react"
+import { useRouter } from 'next/router';
+
+
+const useQueryState = ({ query, defaultValue, options = {} }) => {
+    const router = useRouter();
+    const [newQueryObj, setNewQueryObj] = useState({});
+    const [pathname, setPathname] = useState("");
+
+    const setQuery = useCallback(
+        value => {
+            if (Object.keys(newQueryObj).length > 0) {
+                const href = {
+                    pathname: pathname,
+                    query: {
+                        ...newQueryObj,
+                        ...(!options.replaceQuery && { [query]: value }),
+                    }
+                }
+                if (options.replaceQuery) router.replace(href, href, { shallow: true })
+                else router.push(href, href, { shallow: true })
+            } else {
+                const href = {
+                    pathname: pathname,
+                    query: {
+                        ...(!options.replaceQuery && { [query]: value }),
+                    }
+                }
+                if (options.replaceQuery) router.replace(href, href, { shallow: true });
+                else router.push(href, href, { shallow: true })
+            }
+        },
+        [router]
+    )
+
+    useEffect(() => {
+        if (router.asPath.indexOf("?") !== -1) {
+            let actualQuery = "?" + router.asPath.split("?")[1];
+            const existingQueries = qs.parse(actualQuery, {
+                ignoreQueryPrefix: true,
+            })
+            setPathname(router.asPath.split("?")[0]);
+            setNewQueryObj(existingQueries)
+        } else {
+            setPathname(router.asPath);
+            setNewQueryObj({});
+        }
+    }, [router]);
+
+
+    return {
+        state: newQueryObj[query] ? newQueryObj[query] : defaultValue,
+        setter: setQuery,
+    }
+}
+
+export default useQueryState;
